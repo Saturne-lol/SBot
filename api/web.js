3c39edb18091b99007e13adaa0081ce91e4703f2
Cleboost <clement.balarot@gmail.com>
2024-06-24 17:50:07 +0200

    hotfix cors error

--- api/web.js
+++ api/web.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const {client} = require("../index");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
